032d38ed5f776117eb405fefdf9a04e287481702
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-05 13:25:05 +0530

    Two player plays Snake and Ladder

--- src/com/bridgelabz/snakeandladder.java
+++ src/com/bridgelabz/snakeandladder.java
@@ -5,38 +5,91 @@ import java.util.Random;
 public class snakeandladder {
     public static void main(String[] args) {
         System.out.println("Welcome Player 1");
+        System.out.println("Welcome Player 2");
         int p1 = 0;
-        int count = 0;
-        while (p1 < 100) {
+        int p2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        while (p1 < 100 && p2 < 100) {
+
+            // PLAYER 1
+
             if (p1 < 0) {
                 p1 = 0;
                 continue;
             }
-            Random ran = new Random();
-            int roll = ran.nextInt(5);
-            int out = roll + 1;
-            if (p1 + out > 100) {
+            Random ran1 = new Random();
+            int play1 = ran1.nextInt(4);
+            int roll1 = ran1.nextInt(5);
+            int out1 = roll1 + 1;
+            System.out.println("PLayer 1 is at position => " + p1);
+            System.out.println("Player 1 rolled => " + out1);
+            System.out.println("  ");
+            if (p1 + out1 > 100) {
                 continue;
             }
-            int play = ran.nextInt(3);
-            if (play == 1) {
-                p1 = p1 - out;
-                System.out.println("Player 1 rolled => "+out);
-                System.out.println("player 1 found a Snake and moved behind");
-            } else if (play == 2) {
-                p1 = p1 + out;
-                System.out.println("Player 1 rolled => "+out);
-                System.out.println("Player 1 moved ahead using Ladder");
+            if (play1 == 1) {
+                p1 = p1 - out1;System.out.println("player 1 found a Snake and moves backward");
+            } else if (play1 == 2) {
+                p1 = p1 + out1;
+                System.out.println("Player 1 moves forward");
+            } else if (play1 == 3) {
+                p1 = p1 + out1;
+                System.out.println("Player 1 found a Ladder and plays again");
+                System.out.println("   ");
+                continue;
             } else {
-                System.out.println("Player 1 rolled => "+out);
-                System.out.println("Player 1 cant move forward");
+                System.out.println("Player 1 cant proceed");
+
             }
-            count++;
-            System.out.println("Number of Die rolls by PLayer 1 => "+count);
+            count1++;
             System.out.println("PLayer 1 is at position => "+p1);
             System.out.println("    ");
+            if (p1 == 100) {
+                break;
+            }
+
+            // PLAYER 2
+
+            if (p2 < 0) {
+                p2 = 0;
+                continue;
+            }
+
+            Random ran2 = new Random();
+            int play2 = ran2.nextInt(4);
+            int roll2 = ran2.nextInt(5);
+            int out2 = roll2 + 1;
+            System.out.println("PLayer 2 is at position => " + p2);
+            System.out.println("Player 2 rolled => " + out2);
+            System.out.println("    ");
+            if (p2 + out2 > 100) {
+                continue;
+            }
+            if (play2 == 1) {
+                p2 = p2 - out2;
+                System.out.println("player 2 found a Snake and moves backward");
+            } else if (play2 == 2) {
+                p2 = p2 + out2;
+                System.out.println("Player 2 moves forward");
+            } else if (play2 == 3) {
+                p2 = p2 + out2;
+                System.out.println("Player 2 found a Ladder and plays again");
+                System.out.println("   ");
+                continue;
+            } else {
+                System.out.println("Player 2 cant proceed");
+            }
+            count2++;
+            System.out.println("PLayer 2 is at position => "+p2);
+            System.out.println("    ");
         }
-        System.out.println("Player 1 has completed the Game");
-        System.out.println("Total number of Die rolled => "+count);
+        if (p1 == 100) {
+        System.out.println("Player 1 Won the Game");
+        System.out.println("Total number of Die rolled by PLayer 1 => "+count1);
+        } else {
+            System.out.println("Player 2 Won the Game");
+            System.out.println("Total number of Die rolled by Player 2 => "+count2);
+        }
     }
 }
