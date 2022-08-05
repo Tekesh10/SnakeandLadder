a8530851b561461ebb83365897e3a1d233ff0f22
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-05 11:07:43 +0530

    Player has to roll exact number to complete

--- src/com/bridgelabz/snakeandladder.java
+++ src/com/bridgelabz/snakeandladder.java
@@ -6,7 +6,7 @@ public class snakeandladder {
     public static void main(String[] args) {
         System.out.println("Welcome Player 1");
         int p1 = 0;
-        while (p1 <= 100) {
+        while (p1 < 100) {
             if (p1 < 0) {
                 p1 = 0;
                 continue;
@@ -14,23 +14,24 @@ public class snakeandladder {
             Random ran = new Random();
             int roll = ran.nextInt(5);
             int out = roll + 1;
-            System.out.println(roll + "  " + out);
+            if (p1 + out > 100) {
+                continue;
+            }
             int play = ran.nextInt(3);
             if (play == 1) {
                 p1 = p1 - out;
-                System.out.println("Player 1 rolled => " + out);
+                System.out.println("Player 1 rolled => "+out);
                 System.out.println("player 1 found a Snake and moved behind");
             } else if (play == 2) {
                 p1 = p1 + out;
-                System.out.println("Player 1 rolled => " + out);
+                System.out.println("Player 1 rolled => "+out);
                 System.out.println("Player 1 moved ahead using Ladder");
             } else {
-                System.out.println("Player 1 rolled => " + out);
+                System.out.println("Player 1 rolled => "+out);
                 System.out.println("Player 1 cant move forward");
             }
-            System.out.println("PLayer 1 is at position => " + p1);
+            System.out.println("PLayer 1 is at position => "+p1);
             System.out.println("    ");
-            p1 += 0;
         }
         System.out.println("Player 1 has completed the Game");
     }
